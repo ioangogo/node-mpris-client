308a194fe33c0df308bf9c237f7c9e8f9fca9666
Ioan Loosley <legit.ioangogo@gmail.com>
2019-05-29 22:30:26 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-mpris-next
+
+mpris-next is a nodejs libary that makes use of dbus-next to allow control of a media player and fetching of now playing information
---
+++ mpris.js
@@ -0,0 +1,14 @@
+const MPRIS_IFACE = 'org.mpris.MediaPlayer2.Player';
+const MPRIS_PATH = '/org/mpris/MediaPlayer2';
+const PROPERTIES_IFACE = 'org.freedesktop.DBus.Properties';
+
+class bus{
+    constructor(){
+        this.players=[];
+    };
+
+    async function update(params) {
+        
+    }
+
+}
