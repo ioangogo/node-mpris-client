a6585c2af0d180beaa458c30b2009084cade3425
Ioan Loosley <legit.ioangogo@gmail.com>
2019-05-30 20:26:49 +0100

    renamed file

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# node-mpris-next
+# node-mpris-client
 
-mpris-next is a nodejs libary that makes use of dbus-next to allow control of a media player and fetching of now playing information
+mpris-client is a nodejs libary that makes use of dbus-next to allow control of a media player and fetching of now playing information, It is only a client as there exists a very competent libary for implementing a player in nodeJS
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var session_dbus = require("dbus-next").sessionBus;
+
 const MPRIS_IFACE = 'org.mpris.MediaPlayer2.Player';
 const MPRIS_PATH = '/org/mpris/MediaPlayer2';
 const PROPERTIES_IFACE = 'org.freedesktop.DBus.Properties';
@@ -5,9 +7,9 @@ const PROPERTIES_IFACE = 'org.freedesktop.DBus.Properties';
 class bus{
     constructor(){
         this.players=[];
-    };
+    }
 
-    async function update(params) {
+    async updatePlayerList() {
         
     }
 
